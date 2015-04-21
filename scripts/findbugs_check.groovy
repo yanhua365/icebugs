2919ebb1d3bba0eafd9c81feace49806efe19e01
yanhua365 <yanhua365@gmail.com>
2015-04-21 10:56:19 +0800

    增加了svn的diff

--- scripts/findbugs_check.groovy
+++ scripts/findbugs_check.groovy
@@ -1,22 +1,28 @@
- ROOT_DIR = ".."
- FINDBUGS_HOME =  System.getenv().get('FINDBUGS_HOME')
- MAVEN_HOME = System.getenv().get('M2_HOME')
- REPORT_DIR = "./findbugs_report"
-
+ROOT_DIR = ".."
+FINDBUGS_HOME =  System.getenv().get('FINDBUGS_HOME')
+MAVEN_HOME = System.getenv().get('M2_HOME')
+REPORT_DIR = "./findbugs_report"
+SUBVERSION_HOME = System.getenv().get('SUBVERSION_HOME')
 
 
 File reportDir = new File(REPORT_DIR)
 //reportDir.deleteOnExit()
-reportDir.mkdir()
+if(!reportDir.exists()) reportDir.mkdir()
 
 def prj = new Project(new File(ROOT_DIR))
 def modules = prj.getModules();
 
 Findbugs findbugs = new Findbugs(FINDBUGS_HOME, REPORT_DIR)
+Subversion subversion = new Subversion(SUBVERSION_HOME, REPORT_DIR)
 
 modules.each{m ->
-    findbugs.check(m);
-    //println(m.name)
+    subversion.diff(m)
+}
+
+//TODO 检查一下是否有变化，过滤出有变化的module给下面的findbugs用
+
+modules.each{m ->
+    findbugs.check(m)
 }
 
 class Findbugs{
@@ -24,21 +30,18 @@ class Findbugs{
     String outputDir
 
     Findbugs(String home, String outputDir){
-       this.home = home
+        this.home = home
         this.outputDir = outputDir
     }
 
-
-    //println "${p.text}"
-
     def check(Module module){
-
         String c = new String(home +File.separator+"bin"+File.separator+"findbugs.bat -textui -output  "+outputDir+File.separator+module.getFindbugsOutputFileName()+ " " + module.getClassesDirPath())
-println(c)
+        println(c)
         Process p = c.execute()
-       //println "${p.text}"
 
-
+        def outputStream = new StringBuffer();
+        p.waitForProcessOutput(outputStream, System.err)
+        println "${outputStream}"
     }
 
 }
@@ -83,17 +86,31 @@ class Module{
         new File(project.dir, name + File.separator + "target" + File.separator + "classes").getAbsolutePath()
     }
 
+    def getSrcDirPath(){
+        new File(project.dir, name + File.separator + "src" + File.separator + "main" + File.separator + "java").getAbsolutePath()
+    }
+
 }
 class Subversion{
+    String home;
+    String outputDir;
+    Subversion(String home,String outputDir){
+        this.home = home;
+        this.outputDir = outputDir
+    }
 
+    def diff(Module module){
+//        String c = new String(home +File.separator+"bin"+File.separator+"svn.exe diff --xml --summarize "+ module.getSrcDirPath() + ">" + outputDir + File.separator + module.getSvnOutputFileName())
+        String c = new String(home +File.separator+"bin"+File.separator+"svn diff "+ module.getSrcDirPath())
+        println(c)
+        Process p = c.execute()
+
+        def outputStream = new StringBuffer();
+        p.waitForProcessOutput(outputStream, System.err)
+        println "${outputStream}"
+    }
 }
-
-
-
-
 
 //Process p="D:\\program\\findbugs-3.0.1\\bin\\findbugs.bat -textui -output a.xml D:\\work\\pop-admin-seller-yddms\\pop-admin-seller-dao\\target\\classes,D:\\work\\pop-admin-seller-yddms\\pop-admin-seller-domain\\target\\classes".execute()
 //println "${p.text}"
-
-
 //mvn dependency:list
